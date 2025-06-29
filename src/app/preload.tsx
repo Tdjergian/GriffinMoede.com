d39666d003d3bd6bd7650636438ec1ac694c30c4
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2025-06-28 17:44:02 -0700

    adding button press images to preload

--- src/app/preload.tsx
+++ src/app/preload.tsx
@@ -12,6 +12,19 @@ const PreloadAssets: FC = (): ReactElement => {
       <link rel="preload" href="Desk/keyboard.png" as="image" />
       <link rel="preload" href="Desk/mouse.png" as="image" />
       <link rel="preload" href="Desk/Bankerlamp.png" as="image" />
+      <link rel="preload" href="Desk/desk.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_0.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_1.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_2.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_3.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_4.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_5.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_6.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_7.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_8.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_9.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_star.png" as="image" />
+      <link rel="preload" href="Phone/phone_pressing_pound.png" as="image" />
     </>
   );
 };
